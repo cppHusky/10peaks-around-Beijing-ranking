d4e289ec8eda5d41a391e7e28d4a19afbff183f0
cppHusky <cpphusky@outlook.com>
2025-06-09 18:08:42 +0800

    fixed outdated tests

--- src/record.rs
+++ src/record.rs
@@ -77,36 +77,36 @@ mod tests{
 	use calamine::{Reader,DataType};
 	#[test]
 	fn open_and_read(){
-		let mut workbook:calamine::Xls<_>=calamine::open_workbook("./sample.xls").expect("Cannot open sample.xls");
+		let mut workbook:calamine::Xlsx<_>=calamine::open_workbook("./sample.xlsx").expect("Cannot open sample.xlsx");
 		let data=workbook.worksheet_range("10峰排行榜").expect("Cannot find sheet `10峰排行榜`");
 		assert_eq!(data.start(),Some((0,0)));
-		assert_eq!(data.end(),Some((15,16)));
-		assert_eq!(data[(2,0)].get_int(),Some(1));
-		assert_eq!(data[(3,1)].get_string(),Some("乙"));
-		assert_eq!(data[(6,10)].as_i64(),Some(1));
-		assert_eq!(data[(7,11)].as_i64(),Some(0));
-		assert_eq!(data[(8,12)].as_i64(),Some(4));
-		assert_eq!(data[(9,13)].as_i64(),Some(0));
-		assert_eq!(data[(10,14)].as_i64(),Some(2));
-		assert_eq!(data[(11,16)].get_string(),Some("是"));
-		assert_eq!(data[(12,1)].get_string(),Some("123456789"));
-		assert_eq!(data[(13,1)].get_string(),Some("2025-01-01"));
-		assert_eq!(data[(14,1)].get_string(),Some("1.2"));
-		assert_eq!(data[(15,1)].get_string(),Some("false"));
+		assert_eq!(data.end(),Some((14,8)));
+		assert_eq!(data[(1,0)].as_i64(),Some(1));
+		assert_eq!(data[(2,1)].get_string(),Some("乙"));
+		assert_eq!(data[(3,2)].as_i64(),Some(2));
+		assert_eq!(data[(4,3)].as_i64(),Some(0));
+		assert_eq!(data[(5,4)].as_i64(),Some(2));
+		assert_eq!(data[(6,5)].as_i64(),Some(1));
+		assert_eq!(data[(7,6)].as_i64(),Some(1));
+		assert_eq!(data[(9,8)].get_string(),Some("否"));
+		assert_eq!(data[(11,1)].get_string(),Some("123456789"));
+		assert_eq!(data[(12,1)].get_string(),Some("2025-01-01"));
+		assert_eq!(data[(13,1)].get_string(),Some("1.2"));
+		assert_eq!(data[(14,1)].get_string(),Some("false"));
 	}
 	#[test]
 	fn parse_record(){
-		let mut workbook:calamine::Xls<_>=calamine::open_workbook("./sample.xls").expect("Cannot open sample.xls");
+		let mut workbook:calamine::Xlsx<_>=calamine::open_workbook("./sample.xlsx").expect("Cannot open sample.xlsx");
 		let data=workbook.worksheet_range("10峰排行榜").expect("Cannot find sheet `10峰排行榜`");
-		let data:calamine::Range<calamine::Data>=data.range((2,0),data.end().unwrap());
+		let data:calamine::Range<calamine::Data>=data.range((1,0),data.end().unwrap());
 		let mut data=data.rows();
 		assert_eq!(
 			record::Record::from_row(data.nth(5).unwrap()),
-			record::Record::from(6,"己".to_string(),[2,0,5,0,1],true)
+			record::Record::from(6,"己".to_string(),[3,0,2,1,1],false)
 		);
 		assert_eq!(
 			record::Record::from_row(data.nth(5).unwrap()),
-			record::Record::from(12,"2025-01-01".to_string(),[0,0,0,0,0],false)
+			record::Record::from(12,"2025-01-01".to_string(),[0,0,0,2,0],false)
 		);
 	}
 	#[test]
