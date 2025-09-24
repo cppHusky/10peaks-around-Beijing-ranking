4a357cd532754174668898862e132eb7587aa9f5
cppHusky <cpphusky@outlook.com>
2025-09-24 14:41:11 +0800

    add special style for 30 mountains

--- utils/graphing.ts
+++ utils/graphing.ts
@@ -112,13 +112,7 @@ const drawGridlines=(svg,unitLen:number,height:number,maxCount:number)=>{
   for(let i=1;i <maxCount+1;i++){
     const x=pieceLeft+unitLen*i;
     svg.append("line")
-      .attr("class",(d)=>{
-        if(i%5===0){
-          return "solid";
-        }else{
-          return "dashed";
-        }
-      })
+      .attr("class",i%5===0?"solid":"dashed")
       .attr("x1",x)
       .attr("y1",headingHeight-pieceSep)
       .attr("x2",x)
@@ -137,7 +131,6 @@ const drawRecords=(svg,data:Data[],unitLen:number)=>{
     if(endCount-startCount===0){
       return;
     }
-    const startPos=pieceLeft+unitLen*startCount;
     const endPos=pieceLeft+unitLen*endCount-pieceHeight/2;
     const path=d3.path();
     path.moveTo(pieceLeft,yOffset);
@@ -166,11 +159,45 @@ const drawRecords=(svg,data:Data[],unitLen:number)=>{
       .style("text-anchor","end")
       .text(record.name);
     const yOffset=headingHeight+i*(pieceHeight+pieceSep);
-    draw(yOffset,record.cumulates[3],record.cumulates[4],colors[4],5);
-    draw(yOffset,record.cumulates[2],record.cumulates[3],colors[3],5);
-    draw(yOffset,record.cumulates[1],record.cumulates[2],colors[2],5);
-    draw(yOffset,record.cumulates[0],record.cumulates[1],colors[1],5);
-    draw(yOffset,0,record.cumulates[0],colors[0],10);
+    if(record.sum===30){
+      const grad=svg.append("defs")
+        .append("linearGradient")
+        .attr("id","grad")
+        .attr("x1","0%")
+        .attr("y1","0%")
+        .attr("x2","100%")
+        .attr("y2","0%");
+      colors.forEach((color,index)=>{
+        const offset=(index/(colors.length-1))*100;
+        grad.append("stop")
+          .attr("offset",`${offset}%`)
+          .style("stop-color",color)
+          .style("stop-opacity",1);
+      });
+      const endPos=pieceLeft+unitLen*30-pieceHeight/2;
+      const path=d3.path();
+      path.moveTo(pieceLeft,yOffset);
+      path.lineTo(endPos,yOffset);
+      path.arc(endPos,yOffset+pieceHeight/2,pieceHeight/2,-Math.PI/2,Math.PI/2);
+      path.lineTo(pieceLeft,yOffset+pieceHeight);
+      path.closePath();
+      svg.append("path")
+        .attr("d",path.toString())
+        .style("fill","url(#grad)");
+      svg.append("text")
+        .attr("x",endPos+pieceHeight/2-unitLen/2)
+        .attr("y",yOffset+pieceHeight*0.9)
+        .style("font-size","18px")
+        .style("text-anchor","middle")
+        .style("font-weight","bold")
+        .text("✓");
+    }else{
+      draw(yOffset,record.cumulates[3],record.cumulates[4],colors[4],5);
+      draw(yOffset,record.cumulates[2],record.cumulates[3],colors[3],5);
+      draw(yOffset,record.cumulates[1],record.cumulates[2],colors[2],5);
+      draw(yOffset,record.cumulates[0],record.cumulates[1],colors[1],5);
+      draw(yOffset,0,record.cumulates[0],colors[0],10);
+    }
     if(record.dongling){
       svg.append("text")
         .attr("x",pieceLeft+unitLen/2)
